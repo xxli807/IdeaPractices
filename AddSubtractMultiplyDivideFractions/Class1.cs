0842022290b4cdcce5fa838a62df56398a355fbf
lxx871030@gmail.com <lxx871030@gmail.com>
2015-11-11 23:15:14 +1100

    add the project file for fraction

---
+++ AddSubtractMultiplyDivideFractions/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddSubtractMultiplyDivideFractions
+{
+    public class Class1
+    {
+    }
+
+
+
+    public class Fraction
+    {
+        private int numberator;
+        private int denominator;
+
+
+        public int Numerator
+        {
+            get { return this.numberator; }
+            set { numberator = value; }
+        }
+
+        public int Denominator
+        {
+            get { return this.denominator; }
+            set { this.denominator = value; }
+        }
+
+
+
+
+    }
+}
---
+++ FractionConsoleProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter the Numerator");
+                //get string from the user
+                var numerator = Console.ReadLine();
+
+                Console.WriteLine("Please enter the Denominator");
+                var denominator = Console.ReadLine();
+
+
+                Console.WriteLine("Please enter the Operation");
+                var operation = Console.ReadLine();
+
+                //validation the input and then keep doing the same thing
+                if (true)
+                {
+                    
+
+                }
+
+
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
